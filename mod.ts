95d8bbf431af3076ec3ac5414b0606116d253556
Lukas Trumm <lukas.trumm@centrum.cz>
2020-07-10 22:58:04 +0200

    Add mod.ts

---
+++ mod.ts
@@ -0,0 +1,6 @@
+export { Directory, DirectoryConfiguration } from "./resources/directory.ts";
+export { LoginShellConfiguration, LoginShell } from "./resources/loginShell.ts";
+export { Symlink, SymlinkConfiguration } from "./resources/symlink.ts";
+export { Configuration } from "./resource.ts";
+export { runConfigurationSet } from "./configuration.ts";
+export { printConfigurationSet } from "./configuration.ts";
