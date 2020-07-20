d27524a33738fc4027d3253ddbcaaf37fe667bc5
Lukas Trumm <lukas.trumm@centrum.cz>
2020-07-20 15:41:04 +0200

    Fix dependencies of dep-graph

--- dep-graph/dep-graph.ts
+++ dep-graph/dep-graph.ts
@@ -1,21 +1,25 @@
-import { digraph, toDot } from "https://deno.land/x/graphviz/mod.ts";
-
-import { fs } from "../deps.ts";
+import { digraph, fs, open, toDot } from "../deps.ts";
 import { htmlPage } from "./html-page.ts";
-import { open } from "../deps.ts";
+import { Configuration } from "../resource.ts";
 
-export async function depGraph(config: any): Promise<void> {
+export function depGraph(config: Configuration[]): void {
   const G = digraph("Execution tree", { rankdir: "LR" }, g => {
     for (const node of config) {
       g.node(node.resource.get(node));
       const dependant = node.resource.get(node);
-      const dependency = node.dependsOn.resource.get(node.dependsOn);
-      if (dependency !== "START") {
-        g.edge([dependency, dependant]);
+      const dependency = node.dependsOn?.resource.get(node.dependsOn);
+      if (dependency) {
+        if (dependency !== "START") {
+          g.edge([dependency, dependant]);
+        }
+      } else {
+        console.error(
+          `Missing 'dependsOn' property on '${node.resource.get(node)}'`
+        );
       }
     }
   });
-  const dot = await toDot(G);
+  const dot = toDot(G);
   const fileName = `/tmp/dsc-${new Date().getTime()}.html`;
   fs.writeFileStrSync(fileName, htmlPage(dot));
   open(fileName);
