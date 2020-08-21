bede129b711ff99f3fa22bffc8f746de89f7e2f5
Lukas Trumm <lukas.trumm@centrum.cz>
2020-08-21 13:30:49 +0200

    Fix typo

--- deps.ts
+++ deps.ts
@@ -30,7 +30,7 @@ export const deno = Deno;
 // user land
 export { digraph, toDot } from "https://deno.land/x/graphviz/mod.ts@v0.2.1";
 export { download } from "https://deno.land/x/download/mod.ts@v1.0.0";
-export { isExecutable as isExecutableFile } from "https://deno.land/x/is_exe/mod.ts@1.0.3";
+export { isExecutable as isExecutableFile } from "https://deno.land/x/is_exe/mod.ts@v1.0.3";
 // export { jsonTree } from "https://deno.land/x/json_tree/mod.ts";
 export { jsonTree } from "https://raw.githubusercontent.com/satty1987/json_tree/master/mod.ts";
 // export { open } from "https://deno.land/x/opener/mod.ts";
